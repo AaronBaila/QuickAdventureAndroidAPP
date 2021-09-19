3c058c0b77b467331530b0bc3fb7f09e863bce30
Aaron Baila <aaronbaila@MacBook-Air-de-Aaron.local>
2021-09-19 14:14:32 +0200

    QA 1.1.1

--- app/src/main/java/com/example/tfgdam/Inicio.java
+++ app/src/main/java/com/example/tfgdam/Inicio.java
@@ -8,15 +8,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Inicio extends AppCompatActivity{
-    CardView btMapa, btMisPlanes, btCerrarSesion, btConfig;
+    //CardView btMapa, btMisPlanes, btCerrarSesion, btConfig;
+
+    ImageView btImagenAjustes, btImagenMapa, btImagenPerfilInicio, btImagenSalirInicio;
 
     FirebaseAuth firebaseAuth;
 
@@ -25,12 +29,14 @@ public class Inicio extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        btMapa =(CardView) findViewById(R.id.btMapa);
-        btMisPlanes =(CardView) findViewById(R.id.btMisPlanes);
-        btCerrarSesion =(CardView) findViewById(R.id.btCerrarSesion);
-        btConfig =(CardView) findViewById(R.id.btConfig);
+        btImagenAjustes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //AUN NO IMPLEMENTADO
+            }
+        });
 
-        btMapa.setOnClickListener(new View.OnClickListener() {
+        btImagenMapa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Inicio.this, Mapa.class);
@@ -38,15 +44,14 @@ public class Inicio extends AppCompatActivity{
             }
         });
 
-        btMisPlanes.setOnClickListener(new View.OnClickListener() {
+        btImagenPerfilInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Inicio.this, MisPlanes.class);
-                startActivity(i);
+                //AUN NO IMPLEMENTADO
             }
         });
 
-        btCerrarSesion.setOnClickListener(new View.OnClickListener() {
+        btImagenSalirInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 firebaseAuth.getInstance().signOut();
@@ -56,11 +61,17 @@ public class Inicio extends AppCompatActivity{
             }
         });
 
-        btConfig.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //AUN NO IMPLEMENTADO
-            }
-        });
+        /* btMapa =(CardView) findViewById(R.id.btMapa);
+        btMisPlanes =(CardView) findViewById(R.id.btMisPlanes);
+        btCerrarSesion =(CardView) findViewById(R.id.btCerrarSesion);
+        btConfig =(CardView) findViewById(R.id.btConfig);
+
+        if (nPlanesBD == 0) {
+            Toast toast = Toast.makeText(getApplicationContext(), "No tienes planes", Toast.LENGTH_SHORT);
+        }
+        for (int i = 1; i <= nPlanesBD; i++) {
+            String iterador = String.valueOf(i);
+            mDatabase2 = FirebaseDatabase.getInstance().getReference("Planes").child(idUser).child(iterador);
+            mDatabase2.addListenerForSingleValueEvent(valueEventListener2);*/
     }
 }
